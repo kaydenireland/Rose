03f2bbb864a3f66b8d8c9cf4e436aad7e8109e0e
Kayden Ireland <kaydenireland@aol.com>
2025-10-07 00:32:10 -0400

    lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use rose::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,26 +12,8 @@ fn main() {
     });
 
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = rose::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
